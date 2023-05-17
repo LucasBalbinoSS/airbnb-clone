79bd7615666823366311675f1ab08c3936ccdb43
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2023-05-17 11:16:11 -0300

    Feat: Sombra no menu de lugares com scroll

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,3 +1,30 @@
-"use strict"
+(() => {
+    'use strict'
 
-console.log('Teste!')
+    document.addEventListener('DOMContentLoaded', () => {
+        topo()
+    })
+
+
+    const topo = () => {
+        const $topoLugares:HTMLElement | null = document.querySelector('.js-topo-lugares')
+
+        sombraAdicionar()
+
+
+        // funcoes
+        function sombraAdicionar() {
+            addEventListener('scroll', () => {
+
+                if (scrollY >= 10) {
+                    $topoLugares?.classList.remove('md:shadow-none')
+                    $topoLugares?.classList.add('shadow-md', 'transition-shadow')
+                }
+                else {
+                    $topoLugares?.classList.add('md:shadow-none')
+                    console.log('LUCAS BALBINO')
+                }
+            })
+        }
+    }
+})()
