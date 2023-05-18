2558722dc8eb4b301d6e8d8ce0f235eeedc0edf9
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2023-05-18 16:40:40 -0300

    Feat: Menu de opções de perfil

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -7,9 +7,14 @@
 
 
     const topo = () => {
+        const tablet:number = 768
+        const $body:HTMLElement | null = document.querySelector('body')
         const $topoLugares:HTMLElement | null = document.querySelector('.js-topo-lugares')
+        const $menuperfilOpcoes:HTMLElement | null = document.querySelector('.js-menu-pefil-opcoes')
+        const $btnPerfilOpcoes:HTMLButtonElement | null = document.querySelector('.js-btn-perfil-opcoes')
 
         sombraAdicionar()
+        perfilOpcoes()
 
 
         // funcoes
@@ -25,5 +30,59 @@
                 }
             })
         }
+
+        function perfilOpcoes() {
+            addEventListener('resize', () => {
+
+                if (innerWidth < tablet) {
+
+                    if (! $menuperfilOpcoes?.classList.contains('hidden')) {
+                        esconder($menuperfilOpcoes)
+                        $btnPerfilOpcoes?.classList.remove('shadow-md')
+                    }
+                }
+            })
+
+            perfilOpcoesMostrar()
+            perfilOpcoesEsconder()
+
+
+            function perfilOpcoesMostrar() {
+                $btnPerfilOpcoes?.addEventListener('click', () => {
+                    $menuperfilOpcoes?.classList.toggle('hidden')
+
+                    if (! $menuperfilOpcoes?.classList.contains('hidden')) {
+                        $btnPerfilOpcoes?.classList.add('shadow-md')
+                    }
+                    else {
+                        $btnPerfilOpcoes?.classList.remove('shadow-md')
+                    }
+                })
+            }
+
+            function perfilOpcoesEsconder() {
+                $body?.addEventListener('click', (evento:any) => {
+
+                    if (evento.target.closest('.js-menu-pefil-opcoes') ||
+                     evento.target.closest('.js-btn-perfil-opcoes')) {
+                        return
+                    }
+    
+                    esconder($menuperfilOpcoes)
+
+                    if ($menuperfilOpcoes?.classList.contains('hidden')) {
+                        $btnPerfilOpcoes?.classList.remove('shadow-md')
+                    }
+                })
+            }
+        }
+    }
+
+    function mostrar($alvo:HTMLElement | null) {
+        $alvo?.classList.remove('hidden')
+    }
+
+    function esconder($alvo:HTMLElement | null) {
+        $alvo?.classList.add('hidden')
     }
 })()
